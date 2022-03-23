e3f72f989b78ecfe9cc2708b3f136a36917b34c6
takiyo <alanwalkerlol049@gmail.com>
2022-03-23 14:34:39 +0700

    Add my code

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require("multer");
+const upload = multer({dest: "files/"});
 require('dotenv').config()
 
 var app = express();
@@ -11,10 +13,21 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.get("/api/hello", (req, res) => {
+    return res.send("Hello");
+})
+
+app.post("/api/fileanalyse", upload.any(), (req, res) => {
+    if (!req.files || !req.files[0]) return res.json({});
+    return res.json({
+        name: req.files[0].originalname,
+        type: req.files[0].mimetype,
+        size: req.files[0].size
+    })
+})
 
 
-
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3060;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+    console.log('Your app is listening on port ' + port)
 });
